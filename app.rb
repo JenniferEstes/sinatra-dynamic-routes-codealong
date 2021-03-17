a2e8a1460e57fad3733554a50d13709f37db9310
Jennifer <jenndestes@gmail.com>
2021-03-16 21:18:28 -0500

    commit tree ba28fb9cb2ecac6b37e8691d7bc38e0aa9419398

--- app.rb
+++ app.rb
@@ -14,5 +14,23 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
+  get "/goodbye/:name" do
+    @user_name = params[:name]
+    "Goodbye, #{@user_name}."
+  end
 
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @num1 = params[:number1]
+    @num2 = params[:number2]
+    if @operation == "add"
+      "#{@num1.to_i + @num2.to_i}"
+    elsif @operation == "subtract"
+      "#{@num1.to_i - @num2.to_i}"
+    elsif @operation == "multiply"
+      "#{@num1.to_i * @num2.to_i}"
+    elsif @operation == "divide"
+      "#{@num1.to_i / @num2.to_i}"
+    end
+  end
 end
